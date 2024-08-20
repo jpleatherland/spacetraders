b2ee7a1d4d845b985e2be79b22efcc877f618172
Jack Leatherland <jpl92@live.co.uk>
2024-08-20 22:42:37 +0100

    upate

--- cmd/main/homepage.go
+++ cmd/main/homepage.go
@@ -7,8 +7,45 @@ import (
 	"github.com/jpleatherland/spacetraders/internal/db"
 )
 
+type ServerStatus struct {
+	Status      string `json:"status"`
+	Version     string `json:"version"`
+	ResetDate   string `json:"resetDate"`
+	Description string `json:"description"`
+	Stats       struct {
+		Agents    int `json:"agents"`
+		Ships     int `json:"ships"`
+		Systems   int `json:"systems"`
+		Waypoints int `json:"waypoints"`
+	} `json:"stats"`
+	Leaderboards struct {
+		MostCredits []struct {
+			AgentSymbol string `json:"agentSymbol"`
+			Credits     int64  `json:"credits"`
+		} `json:"mostCredits"`
+		MostSubmittedCharts []struct {
+			AgentSymbol string `json:"agentSymbol"`
+			ChartCount  int    `json:"chartCount"`
+		} `json:"mostSubmittedCharts"`
+	} `json:"leaderboards"`
+	ServerResets struct {
+		Next      string `json:"next"`
+		Frequency string `json:"frequency"`
+	} `json:"serverResets"`
+	Announcements []struct {
+		Title string `json:"title"`
+		Body  string `json:"body"`
+	} `json:"announcements"`
+	Links []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"links"`
+}
+
 func (resources *Resources) homePage(rw http.ResponseWriter, req *http.Request, session db.Session) {
 	tmpl := template.Must(template.ParseFiles("views/homepage.html"))
+
+	resp := http.Get("")
 
 	err := tmpl.Execute(rw, nil)
 	if err != nil {
